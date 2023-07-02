9b68e6f74a26ba3ea2947479a6ed069c906d2bf5
Boyarkina Darya <bo_dary@mail.ru>
2023-07-02 22:57:00 +0700

    Init Allure in project

--- tests/db/test_crud.py
+++ tests/db/test_crud.py
@@ -1,3 +1,10 @@
+"""Тесты CRUD User через DB"""
+
+import pytest
+import json
+import allure
+from allure_commons.types import Severity, AttachmentType
+
 from src.db_entity.User.db_func import UserDBFunc
 
 USER_DATA_CREATE = {
@@ -29,28 +36,46 @@ USER_DATA_UPDATE = {
 }
 
 
+@allure.severity(Severity.BLOCKER)
+@allure.feature('DB-тесты для сущности User')
+@allure.story('Тесты на CRUD')
+@allure.title('Самый простой, позитивный тест')
+@pytest.mark.positive
+@pytest.mark.db
+@pytest.mark.crud
+@pytest.mark.user
 def test_crud(db_client):
     """Тест CRUD для User через запросы к БД"""
     # ---------- CREATE USER ----------
-    UserDBFunc.create(db_client, USER_DATA_CREATE)
+    with allure.step("Create User"):
+        allure.attach(json.dumps(USER_DATA_CREATE), name="data_for_create", attachment_type=AttachmentType.JSON)
+        UserDBFunc.create(db_client, USER_DATA_CREATE)
     # ---------- GET USER ----------
-    user_create = UserDBFunc.get_by_id(db_client, USER_DATA_CREATE["id"])
-    tuple_user_create = tuple(user_create[0])
-    id_user_create = tuple_user_create[0]
-    tuple_user_data_create = tuple(USER_DATA_CREATE.values())
-    assert tuple_user_data_create == tuple_user_create, f"tuple_user_data_create {tuple_user_data_create} not equal tuple_user_create {tuple_user_create}"
+    with allure.step("Get User after Create and check it"):
+        user_create = UserDBFunc.get_by_id(db_client, USER_DATA_CREATE["id"])
+        allure.attach(json.dumps(user_create), name="data_from_db", attachment_type=AttachmentType.JSON)
+        tuple_user_create = tuple(user_create[0])
+        id_user_create = tuple_user_create[0]
+        tuple_user_data_create = tuple(USER_DATA_CREATE.values())
+        assert tuple_user_data_create == tuple_user_create, f"tuple_user_data_create {tuple_user_data_create} not equal tuple_user_create {tuple_user_create}"
     # ---------- UPDATE USER ----------
-    UserDBFunc.update_by_id(db_client, USER_DATA_CREATE["id"], USER_DATA_UPDATE)
+    with allure.step("Update User"):
+        UserDBFunc.update_by_id(db_client, USER_DATA_CREATE["id"], USER_DATA_UPDATE)
+        allure.attach(json.dumps(USER_DATA_UPDATE), name="data_for_update", attachment_type=AttachmentType.JSON)
     # ---------- GET USER ----------
-    user_update = UserDBFunc.get_by_id(db_client, USER_DATA_CREATE["id"])
-    tuple_user_update = tuple(user_update[0])
-    tuple_user_data_update = tuple(USER_DATA_UPDATE.values())
-    id_user_update = tuple_user_update[0]
-    assert id_user_update == id_user_create, f"id_user_update {id_user_update} not equal id_user_create {id_user_create}"
-    assert tuple_user_data_update == tuple_user_update[
-                                     1:], f"tuple_user_data_update {tuple_user_data_update} not equal tuple_user_update {tuple_user_update}"
+    with allure.step("Get User after Update and check it"):
+        user_update = UserDBFunc.get_by_id(db_client, USER_DATA_CREATE["id"])
+        allure.attach(json.dumps(user_update), name="data_from_db", attachment_type=AttachmentType.JSON)
+        tuple_user_update = tuple(user_update[0])
+        tuple_user_data_update = tuple(USER_DATA_UPDATE.values())
+        id_user_update = tuple_user_update[0]
+        assert id_user_update == id_user_create, f"id_user_update {id_user_update} not equal id_user_create {id_user_create}"
+        assert tuple_user_data_update == tuple_user_update[
+                                         1:], f"tuple_user_data_update {tuple_user_data_update} not equal tuple_user_update {tuple_user_update}"
     # ---------- DELETE USER ----------
-    UserDBFunc.delete_by_id(db_client, USER_DATA_CREATE["id"])
+    with allure.step("Delete User"):
+        UserDBFunc.delete_by_id(db_client, USER_DATA_CREATE["id"])
     # ---------- GET USER ----------
-    assert len(
-        UserDBFunc.get_by_id(db_client, USER_DATA_CREATE["id"])) == 0, "Error: delete User from table UNSUCCESSFUL!"
+    with allure.step("Get User after Update and check it"):
+        assert len(
+            UserDBFunc.get_by_id(db_client, USER_DATA_CREATE["id"])) == 0, "Error: delete User from table UNSUCCESSFUL!"
