e2ff9f8f1f2e48262e96e1e62200037b474e9a39
Boyarkina Darya <bo_dary@mail.ru>
2023-07-02 14:51:24 +0700

    Add conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,24 @@
 """
     Общие фикстуры для тестов
 """
+import os
 import pytest
+
+import src.config  # чтобы подгрузились env-переменные
+from src.db_client import DBClient
+
+
+@pytest.fixture(scope="session")
+def db_client():
+    """Получаем клиента к БД"""
+    db_credential = (
+        os.getenv("DB_NAME"),
+        os.getenv("DB_USER"),
+        os.getenv("DB_PASSWORD"),
+        os.getenv("DB_HOST"),
+        os.getenv("DB_PORT")
+    )
+    db_client = DBClient()
+    db_client.create_connection(*db_credential)
+    yield db_client
+    db_client.close_connection()
